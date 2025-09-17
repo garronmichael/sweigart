1b5d5e1b49df6ae8cf853b45df0877931d783c62
Garron Sanchez <garron.michael@gmail.com>
2025-09-16 22:33:49 -0700

    add chapter 1 practice questions

---
+++ atbs/chapter_01/practice_questions.py
@@ -0,0 +1,35 @@
+"""
+1.
+operators:
+*
+-
+/
++
+
+values:
+'hello'
+-88.8
+5
+
+2. spam is a variable while 'spam' is a string
+
+3. Some python data types are str, int, float, bool
+
+4. An expression is made of an operator and values. All expressions evaluate to a single value.
+
+5. An expression is a piece of code that evaluates to a single value while a statement is an instruction that can be
+    executed.
+
+6. 20
+
+7. 'spamspamspam'
+
+8. 100 is not a valid variable name because a valid variable name cannot start with a number. eggs is a valid varialble
+    name because it begins with a letter, contains only letters, numbers and underscores, does not contain space and
+    is not a python keyword.
+
+9. str(), int(), float()
+
+10. 'I eat ' + 99 + ' burritos.' causes an error because python does not have implicit type coercion. This is fixed by
+    explicit type coercion: 'I eat ' + str(99) + ' burritos.'
+"""
