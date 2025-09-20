c643e05c2a9206dc5b4aa30dc1551d7fd4d69a20
Garron Sanchez <garron.michael@gmail.com>
2025-09-20 16:55:16 -0700

    add chapter 3 practice questions

---
+++ atbs/chapter_03/practice_questions.py
@@ -0,0 +1,18 @@
+# 1. ctrl + c
+
+# 2. break will exit the loop while continue will return to the loop's condition
+
+# 3. range(10) is 0 – 9, range(0, 10) is 0 – 9, range(0, 10, 1) is 0 – 9 so they are all the same
+
+# 4.
+
+for i in range(10):
+    print(i)
+
+i = 0
+
+while i < 10:
+    print(i)
+    i = i + 1
+
+# 5. spam.bacon()
